635575cca021a3244094aa67d488bb1298b71b5b
Kenny Pflug <kenny.pflug@live.de>
2024-03-03 20:14:55 +0100

    feat: include non-working example for AutoMapper
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

--- WebApp/ToDo/GetToDos/GetToDosEndpoint.cs
+++ WebApp/ToDo/GetToDos/GetToDosEndpoint.cs
@@ -1,7 +1,10 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using WebApp.DatabaseAccess.Model;
 
 namespace WebApp.ToDo.GetToDos;
 
@@ -15,11 +18,12 @@ public static class GetToDosEndpoint
 
     public static async Task<IResult> GetToDos(
         IGetToDosSession session,
+        IMapper mapper,
         CancellationToken cancellationToken = default
     )
     {
         var toDoList = await session.GetToDoListAsync(cancellationToken);
-        var dtoList = ToDoListDto.FromToDoItems(toDoList);
+        var dtoList = mapper.Map<List<ToDoItem>, List<ToDoListDto>>(toDoList);
         return Results.Ok(dtoList);
     }
 }
---
+++ WebApp/ToDo/GetToDos/ToDoListProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using WebApp.DatabaseAccess.Model;
+
+namespace WebApp.ToDo.GetToDos;
+
+public sealed class ToDoListProfile : Profile
+{
+    public ToDoListProfile()
+    {
+        CreateMap<ToDoItem, ToDoListDto>();
+    }
+}
